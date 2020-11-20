ca849a36bfc5114aac2b5c9c69e1a28b00fbd38b
kuxk <podhajnymaciejkolo2@gmail.com>
2020-11-20 11:15:17 +0100

    gra

---
+++ Assets/ruch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ruch : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        
+        animator.SetFloat("vertical",Input.GetAxis("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.LeftShift ))
+        {
+            run();
+        }
+        
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            animator.SetBool("bieg", false);
+        }
+
+    }
+    public void Jump()
+    {
+        animator.SetTrigger("jump");
+    }
+    public void run()
+    {
+        animator.SetBool("bieg",true);
+    }
+    
+    
+}
