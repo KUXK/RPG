f28996b5110b98b93a534c8431b2d52fc4866a1e
kuxk <podhajnymaciejkolo2@gmail.com>
2020-12-04 10:06:45 +0100

    ANIMACJA

--- Assets/ruch.cs
+++ Assets/ruch.cs
@@ -5,30 +5,83 @@ using UnityEngine;
 public class ruch : MonoBehaviour
 {
     Animator animator;
+    [SerializeField] GameObject[] skillParticles;
+    private IEnumerator coroutine;
     void Start()
     {
         animator = GetComponent<Animator>();
+        for(int i=0; i < skillParticles.Length; i++)
+        {
+            skillParticles[i].SetActive(false);
+        }
     }
 
-    
+
     void Update()
     {
-        
-        animator.SetFloat("vertical",Input.GetAxis("Vertical"));
+
+        animator.SetFloat("vertical", Input.GetAxis("Vertical"));
         animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
         if (Input.GetKeyUp(KeyCode.Space))
         {
             Jump();
         }
-        if (Input.GetKey(KeyCode.LeftShift ))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             run();
         }
-        
+
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             animator.SetBool("bieg", false);
         }
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            UseSkill(1);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            UseSkill(2);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            UseSkill(3);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            UseSkill(4);
+        }
+
+    }
+    void UseSkill(int skillNumber)
+    {
+        animator.SetTrigger("skill" + skillNumber);
+        skillParticles[skillNumber - 1].SetActive(true);
+        switch(skillNumber)
+        {
+            case 1:
+                coroutine = WaitRoEnableObject(skillParticles[skillNumber - 1], 3);
+                break;
+            case 2:
+                coroutine = WaitRoEnableObject(skillParticles[skillNumber - 1], 2);
+                break;
+            case 3:
+                coroutine = WaitRoEnableObject(skillParticles[skillNumber - 1], 2);
+                break;
+            case 4:
+                coroutine = WaitRoEnableObject(skillParticles[skillNumber - 1], 2);
+                break;
+
+        }
+
+        
+        StartCoroutine(coroutine);
+
+    }
+    IEnumerator WaitRoEnableObject(GameObject obj, float time)
+    {
+        yield return new WaitForSeconds(time);
+        obj.SetActive(false);
 
     }
     public void Jump()
